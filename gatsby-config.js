759afc52dd94999845218951e8b0c6ed67c476fa
David Holmlund <david.holmlund@gmail.com>
2020-05-30 14:11:41 +0200

    Updated name and analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,15 +37,15 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingId: "UA-2221532-5",
         head: true,
       }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `David Holmlund - Webbutveckling & Framtidens Teknik`,
+        short_name: `David Holmlund`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
